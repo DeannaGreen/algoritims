7d19f0f3b30071fb71f6e592b9257153fcf41ea0
Deanna Green <deanna@16vmini.co.uk>
2019-05-08 10:26:00 +0100

    added a seperate graph page for the algorithms that i have created

--- app.py
+++ app.py
@@ -65,61 +65,54 @@ def shuffle_time():
 
 @app.route("/my_shuffle")
 def my_shuffle():
-  x = randint(1,100)
-  x_co_ord.append(x)
-  list = numpy.random.random_integers(1, 100, x)
-  current_time = time.time()
-  print(list)
-  for i in range((len(list) -1), 0, -1):
-      j = random.randint(0, (i + 1))
-      list[i], list[j] = list[j], list[i]
-  print(list)
-  time_now = time.time()
-  final_time = time_now - current_time
-  y_co_ord.append(final_time)
-  print(x_co_ord)
-  print(y_co_ord)
-  return str(final_time) + " seconds and " + str(x) + " elements in array"
+  co_ords = []
+  for k in range (0, 1001, +50):
+      list = numpy.random.random_integers(1, 100, k)
+      current_time = time.time()
+      print(list)
+      for i in range((len(list) -1), 0, -1):
+          j = random.randint(0, (i + 1))
+          list[i], list[j] = list[j], list[i]
+      print(list)
+      time_now = time.time()
+      final_time = time_now - current_time
+      co_ords.append([k, final_time])
+  return jsonify(co_ords)
 
 @app.route("/my_last")
 def my_last():
-  x = randint(1,100)
-  x_co_ord.append(x)
-  list = numpy.random.random_integers(1, 100, x)
-  current_time = time.time()
-  list[-1]
-  time_now = time.time()
-  final_time = time_now - current_time
-  y_co_ord.append(final_time)
-  print(x_co_ord)
-  print(y_co_ord)
-  return str(final_time) + " seconds and " + str(x) + " elements in array"
+  co_ords = []
+  for i in range (1, 1001, +50):
+      list = numpy.random.random_integers(1, 100, i)
+      current_time = time.time()
+      list[-1]
+      time_now = time.time()
+      final_time = time_now - current_time
+      co_ords.append([i, final_time])
+  return jsonify(co_ords)
 
 @app.route("/my_reverse")
 def my_reverse():
-  x = randint(1,100)
-  x_co_ord.append(x)
-  list = numpy.random.random_integers(1, 100, x)
-  current_time = time.time()
-  print(list)
-  i = 0            # first item
-  j = len(list)-1   # last item
-  while i < j:
-      list[i],list[j] = list[j],list[i]
-      i += 1
-      j -= 1
-  print(list)
-  time_now = time.time()
-  final_time = time_now - current_time
-  y_co_ord.append(final_time)
-  print(x_co_ord)
-  print(y_co_ord)
-  return str(final_time) + " seconds and " + str(x) + " elements in array"
+  co_ords = []
+  for k in range (1, 1001, +50):
+      list = numpy.random.random_integers(1, 100, k)
+      current_time = time.time()
+      # print(list)
+      i = 0            # first item
+      j = len(list)-1   # last item
+      while i < j:
+          list[i],list[j] = list[j],list[i]
+          i += 1
+          j -= 1
+      # print(list)
+      time_now = time.time()
+      final_time = time_now - current_time
+      co_ords.append([k, final_time])
+  return jsonify(co_ords)
 
 @app.route("/students")
 def students():
     names = ["john", "bob", "madison", "jenny"]
-    x_co_ord.append(len(names))
     current_time = time.time()
     for name in range(0, (len(names) -1), +1):
         count = 0
@@ -131,15 +124,11 @@ def students():
                 count += 1
     time_now = time.time()
     final_time = time_now - current_time
-    y_co_ord.append(final_time)
-    print(x_co_ord)
-    print(y_co_ord)
     return "yay"
 
 @app.route("/dupe")
 def dupe():
     words = ["car", "dog", "cat", "car", "fish", "dog"]
-    x_co_ord.append(len(words))
     current_time = time.time()
     for word in range(0, (len(words) -1), +1):
         count = 0
@@ -156,14 +145,20 @@ def dupe():
                 count += 1
     time_now = time.time()
     final_time = time_now - current_time
-    y_co_ord.append(final_time)
-    print(x_co_ord)
-    print(y_co_ord)
     return "yay"
 
 @app.route("/graph")
 def graph():
     return render_template ("graph.html")
 
+@app.route("/graph2")
+def graph2():
+    return render_template ("graph2.html")
+
+def test_sum():
+    assert sum([1, 2, 3]) == 6, "Should be 6"
+
 if __name__ == "__main__":
+  test_sum()
+  print("Everything passed")
   app.run()
